745d4294e06660f57dab71638727c0a4e0b99a42
fengxinqiang <fengxinqiang@bytedance.com>
2018-04-02 17:22:06 +0800

    增加截取字符串

--- index.js
+++ index.js
@@ -9,4 +9,24 @@ measureText.prototype.setFont = function(font){
 measureText.prototype.width = function(text){
 	return this.ctx.measureText(text).width;
 }
+/**
+*text (string), aimwidth (number), [ellipsis] (string)
+*/
+measureText.prototype.cutByWidth = function(text, aimwidth, ellipsis){
+	if(!text || !aimwidth || aimwidth <= 0 ){
+		return '';
+	}
+	let w = this.width(text);
+	let ellipsisw = this.width(ellipsis||'');
+	let cutw = aimwidth - ellipsisw;
+	let avgw = w/text.length;
+	let len = parseInt(cutw / avgw);
+	let overflow = this.width( text.substring(0, len) ) > cutw; 
+	if(overflow){
+		while(this.width( text.substring(0, --len) ) > cutw){};		
+	}else{
+		while(this.width( text.substring(0, -len) ) > cutw){};		
+	}
+	return `${text.substring(0,len)}${ellipsis}`;
+}
 module.exports = measureText;
